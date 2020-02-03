7e23d2468babe376906b89090dc895cf910e52d3
wish13t <vishal13joshi@gmail.com>
2020-02-04 00:24:34 +0530

    Finish Travel Blog

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+  class Blog < ActiveRecord::Base
+  validates :content, :length=>{ :maximum=>500 }
+  belongs_to :travel
+end
+  class Travel < ActiveRecord::Database
+    has_many :blogs
+  end
